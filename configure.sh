01869dcff8874ed623395a4f5fef9d2f27e4c07a
Vincent Hardouin <vincenthardouin@icloud.com>
2021-07-30 18:19:47 +0200

    Add password authentication

--- configure.sh
+++ configure.sh
@@ -2,6 +2,7 @@
 
 read -p "What's your Time Capsule IP Address ?" time_capsule_address
 read -s -p "What's your Time Capsule Disk password ?" time_capsule_password
+
 echo ""
 echo "{\"address\":\"${time_capsule_address}\", \"password\":\"${time_capsule_password}\"}" > time-capsule.json
 
@@ -10,5 +11,11 @@ rm -rf /mnt/timecapsule 2>/dev/null
 
 mkdir /mnt/timecapsule
 mount.cifs //${time_capsule_address}/Data /mnt/timecapsule/ -o password="${time_capsule_password}",sec=ntlm,vers=1.0
+
+echo "Let's create an account."
+read -p "What username do you want ?" account_username
+echo -n "${account_username}:" >> ./nginx/.htpasswd
+echo "What password do you want ?"
+openssl passwd -apr1 >> ./nginx/.htpasswd
 
 echo "Time Capsule mounted and configuration saved"
